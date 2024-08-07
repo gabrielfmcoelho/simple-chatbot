0fc35fa4d5a582f6aeec30ffe7f462f0c2a6c410
ugur <hi@ugurogen.com>
2024-08-07 20:56:45 +0300

    first

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  basePath: "/dashboard",
   assetPrefix: isProd
     ? "https://dashboard.shadcnuikit.com/dashboard"
     : undefined,
