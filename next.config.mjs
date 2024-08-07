635020e670b81d7f4087ce69c22cf7dc12b413bd
ugur <hi@ugurogen.com>
2024-08-07 18:08:46 +0300

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites() {
-    return {
-      beforeFiles: [
-        // if the host is `app.acme.com`,
-        // this rewrite will be applied
-        {
-          source: "/:path*",
-          has: [
-            {
-              type: "host",
-              value: "dashboard.shadcnuikit.com",
-            },
-          ],
-          destination: "/dashboard/:path*",
-        },
-      ],
-    };
-  },
+  basePath: "/dashboard",
 };
 
 export default nextConfig;
