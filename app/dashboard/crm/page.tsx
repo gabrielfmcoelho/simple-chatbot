0277798b747f776908e1534dfb702832fc561929
ugur <hi@ugurogen.com>
2024-08-20 02:44:00 +0300

    new dashboards

--- app/dashboard/crm/page.tsx
+++ app/dashboard/crm/page.tsx
@@ -1,12 +1,3 @@
-import { TotalCustomersCard } from "./total-customers";
-import { TargetCard } from "./target-card";
-import { TotalRevenueCard } from "./total-revenue";
-import { ConversionRatioCard } from "./conversion-ratio";
-import CalendarDateRangePicker from "@/components/dashboard/ecommerce/date-range-picker";
-import { Button } from "@/components/ui/button";
-import { LeadsBySourceCard } from "./leads-by-source";
-import { LeadsCard } from "./leads";
-import { LeadsByCountryCard } from "./leads-by-country";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -16,29 +7,5 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return (
-    <>
-      <div className="mb-4 flex items-center justify-between space-y-2">
-        <h1 className="text-2xl font-bold tracking-tight">CRM Dashboard</h1>
-        <div className="flex items-center space-x-2">
-          <CalendarDateRangePicker />
-          <Button>Download</Button>
-        </div>
-      </div>
-      <div className="space-y-4">
-        <div className="grid grid-cols-4 gap-4">
-          <TargetCard />
-          <TotalCustomersCard />
-          <TotalRevenueCard />
-          <ConversionRatioCard />
-        </div>
-        <div className="grid grid-cols-3 gap-4">
-          <LeadsBySourceCard />
-          <LeadsByCountryCard className="col-span-2" />
-        </div>
-
-        <LeadsCard className="col-span-2" />
-      </div>
-    </>
-  );
+  return <>crm</>;
 }
