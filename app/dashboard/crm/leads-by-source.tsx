08cb470bb72556fdf4c802bb9dbe3e03d584f21d
ugur <hi@ugurogen.com>
2024-08-20 10:02:10 +0300

    new dashboards

--- app/dashboard/crm/leads-by-source.tsx
+++ app/dashboard/crm/leads-by-source.tsx
@@ -1,23 +1,13 @@
 "use client";
 
 import * as React from "react";
-import { TrendingUp } from "lucide-react";
 import { Label, Pie, PieChart } from "recharts";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle
-} from "@/components/ui/card";
 import {
   ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent
 } from "@/components/ui/chart";
-import { CardOptionsMenu } from "@/components/CardActionMenus";
 
 const chartData = [
   { browser: "new", tickets: 40, fill: "var(--color-new)" },
@@ -37,51 +27,34 @@ const chartConfig = {
 
 export function LeadsBySourceCard() {
   return (
-    <Card className="flex flex-col">
-      <CardHeader className="flex flex-row justify-between">
-        <CardTitle>Leads by Source</CardTitle>
-        <CardOptionsMenu />
-      </CardHeader>
-      <CardContent className="flex-1">
-        <ChartContainer config={chartConfig} className="mx-auto aspect-square max-h-[350px]">
-          <PieChart>
-            <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
-            <Pie
-              data={chartData}
-              dataKey="tickets"
-              nameKey="browser"
-              innerRadius={60}
-              strokeWidth={5}>
-              <Label
-                content={({ viewBox }) => {
-                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                    return (
-                      <text
-                        x={viewBox.cx}
-                        y={viewBox.cy}
-                        textAnchor="middle"
-                        dominantBaseline="middle">
-                        <tspan
-                          x={viewBox.cx}
-                          y={viewBox.cy}
-                          className="fill-foreground text-3xl font-bold">
-                          88%
-                        </tspan>
-                        <tspan
-                          x={viewBox.cx}
-                          y={(viewBox.cy || 0) + 24}
-                          className="fill-muted-foreground">
-                          Completed
-                        </tspan>
-                      </text>
-                    );
-                  }
-                }}
-              />
-            </Pie>
-          </PieChart>
-        </ChartContainer>
-      </CardContent>
-    </Card>
+    <ChartContainer config={chartConfig} className="mx-auto aspect-square max-h-[350px]">
+      <PieChart>
+        <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+        <Pie data={chartData} dataKey="tickets" nameKey="browser" innerRadius={60} strokeWidth={5}>
+          <Label
+            content={({ viewBox }) => {
+              if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                return (
+                  <text x={viewBox.cx} y={viewBox.cy} textAnchor="middle" dominantBaseline="middle">
+                    <tspan
+                      x={viewBox.cx}
+                      y={viewBox.cy}
+                      className="fill-foreground text-3xl font-bold">
+                      88%
+                    </tspan>
+                    <tspan
+                      x={viewBox.cx}
+                      y={(viewBox.cy || 0) + 24}
+                      className="fill-muted-foreground">
+                      Completed
+                    </tspan>
+                  </text>
+                );
+              }
+            }}
+          />
+        </Pie>
+      </PieChart>
+    </ChartContainer>
   );
 }
