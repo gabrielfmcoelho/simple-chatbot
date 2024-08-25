fc6b88be2101e0536c4706fd151d92399f945db5
ugur <hi@ugurogen.com>
2024-08-25 14:47:57 +0300

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,8 @@
 import { NextResponse, type NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-
-  const { url, nextUrl } = request;
-  const { host, hostname, pathname } = nextUrl;
-
-  if (host === "dashboard.shadcnuikit.com") {
-    return NextResponse.redirect(new URL(pathname, "https://shadcnuikit.com"));
-  }
-
-  // console.log("request.url", new URL(pathname, "https://shadcnuikit.com").toString());
-
-  return response;
+  return NextResponse.redirect(new URL("/dashboard/default", request.url));
 }
 
 export const config = {
-  matcher: "/:path*"
+  matcher: ["/dashboard", "/"]
 };
