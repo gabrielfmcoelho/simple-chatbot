2392d374668b78d6dcd7838d733f0e609a366735
toby <contact@bundui.com>
2024-10-23 00:43:27 +0300

    fix: Theme customization panel added (v1)

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { SelectedChatContext, ThemeOptionsContext } from "./contexts";
 import { ThemeOptionsProps } from "@/types/theme";
 import { ChatItemProps } from "@/app/dashboard/(auth)/apps/chat/types";
-import { themeColors } from "@/store/useThemeStore";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [selectedChat, setSelectedChat] = useState<ChatItemProps | null>(null);
@@ -20,27 +19,6 @@ export default function Providers({ children }: { children: React.ReactNode }) {
     rounded: "lg",
     shadow: "none"
   });
-
-  useEffect(() => {
-    const store = localStorage.getItem("settings-storage");
-    const storagedTheme = store ? JSON.parse(store)?.state : null;
-    if (storagedTheme) {
-      document.documentElement.style.setProperty(
-        "--primary",
-        themeColors[storagedTheme.colorScheme]
-      );
-      document.addEventListener("DOMContentLoaded", function () {
-        if (storagedTheme.fontFamily) {
-          const v = "var(--font-" + storagedTheme.fontFamily + ")";
-          document.body.style.setProperty("font-family", v);
-        }
-
-        if (storagedTheme.contentContainer) {
-          document.querySelector("main")?.classList.add("container", "mx-auto");
-        }
-      });
-    }
-  }, []);
 
   return (
     <ThemeOptionsContext.Provider value={{ themeOptions, setThemeOptions }}>
