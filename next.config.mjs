8788424cb0bfd14dafebd4760c345ddd3a482096
toby <contact@bundui.com>
2024-11-12 00:46:40 +0300

    uptated next config file

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ config();
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  assetPrefix: isProd ? "https://dashboard.shadcnuikit.com" : undefined,
+  // assetPrefix: isProd ? "https://dashboard.shadcnuikit.com" : undefined,
   env: {
     BASE_URL: process.env.BASE_URL,
     DASHBOARD_BASE_URL: process.env.DASHBOARD_BASE_URL
