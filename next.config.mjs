1264df76075805ab651e6c1179dafa4ac330e8dc
toby <contact@bundui.com>
2025-02-23 00:59:32 +0300

    chore: update to Next.js 15 and React 19

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,10 @@ import { config } from "dotenv";
 
 config();
 
+const isProd = process.env.NODE_ENV === "prod";
+
 const nextConfig = {
-  assetPrefix: "https://dashboard.shadcnuikit.com",
+  assetPrefix: isProd ? "https://dashboard.shadcnuikit.com" : undefined,
   env: {
     BASE_URL: process.env.BASE_URL,
     DASHBOARD_BASE_URL: process.env.DASHBOARD_BASE_URL
