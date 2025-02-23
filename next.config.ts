f25c23587b6e0c79e729d5af7985893c0064e72e
toby <contact@bundui.com>
2025-02-23 09:42:46 +0300

    next.js config

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,10 @@ import { config } from "dotenv";
 
 config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  assetPrefix: "https://dashboard.shadcnuikit.com",
+  assetPrefix: isProduction ? process.env.DASHBOARD_BASE_URL : undefined,
   env: {
     BASE_URL: process.env.BASE_URL,
     DASHBOARD_BASE_URL: process.env.DASHBOARD_BASE_URL
