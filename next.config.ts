b675ac780e547f76981ed7988b85b167ee1ebb43
toby <contact@bundui.com>
2025-06-25 12:57:58 +0300

    chore(deps): update project dependencies and some trivial things

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  assetPrefix: isProduction ? "https://dashboard.shadcnuikit.com" : undefined,
   images: {
     remotePatterns: [
       {
